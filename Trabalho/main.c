94f9d1c7709753dc292f8071c8025f19896dc0a0
Catterina Vittorazzi Salvador <102424572+catterinavs@users.noreply.github.com>
2023-01-09 17:24:36 -0300

    Listas de músicas(Vet de 30) e sistemas para isso (leitura das inf)

--- Trabalho/main.c
+++ Trabalho/main.c
@@ -1,5 +1,18 @@
+/*
+O que já foi feito:
+- Lista da pesquisa
+- Vetor de 30 espaços para as músicas
+
+Próxima coisa
+- Shellsort nas listas
+- Listas das pessoas que ...(olhar no trab)
+- Módulo para arquivo 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+
+#define MAX 30
 
 typedef struct pesquisa {
     char nome[40];
@@ -13,8 +26,21 @@ typedef struct lista{
     struct lista *prox;
 }Lista;
 
+typedef struct listaMusicas{
+    int id;
+    int numVotos;
+}ListaMusicas;
+
 void linha (){
-    printf ("---------------------------------------------------------\n");
+    printf ("-------------------------------------------\n");
+}
+
+void limpa(){
+    system("cls");
+}
+
+void n(){
+    printf("\n");
 }
 
 void digite(){
@@ -25,12 +51,16 @@ void buf(){
     setbuf(stdin, NULL);
 }
 
+int estaVazia(Lista **l){
+    return (*l == NULL);
+}
+
 Lista* criarLista (){
     return NULL;
 }
 
 //Cria uma entrada na lista de pesquisa
-Lista* CriarEntrada(){
+Lista* criarEntrada(){
     Lista *n = malloc(sizeof(Lista));
     int i;
 
@@ -59,7 +89,7 @@ Lista* CriarEntrada(){
 }
 
 //Adiciona a entrada na lista correta
-void AdicionaLista(Lista **l, Lista *n){
+void adicionaLista(Lista **l, Lista *n){
     if (*l == NULL){
         *l = n;
     }
@@ -70,52 +100,113 @@ void AdicionaLista(Lista **l, Lista *n){
 }
 
 void imprime(Lista **l){
-    for (Lista *p = (*l); p!=NULL; p = p->prox){
-        printf("%s\t", p->info.nome);
-        printf("%d\t", p->info.idade);
-        printf("%c\t", p->info.sexo);
-        for(int i = 0; i < 5; i++){
-            printf("%d ", p->info.musicas[i]);
+    if(!estaVazia(l)){
+        for (Lista *p = (*l); p!=NULL; p = p->prox){
+            printf("%s\t", p->info.nome);
+            printf("%d\t", p->info.idade);
+            printf("%c\t", p->info.sexo);
+            for(int i = 0; i < 5; i++){
+                printf("%d ", p->info.musicas[i]);
         }
-    linha();
+        n();
+        linha();
+        }
+    }
+    else {
+        printf("Lista vazia!!!\n");
     }
 }
 
+void iniciaListaMusicas (ListaMusicas *lm){
+    for(int i = 0; i < MAX; i++){
+        lm[i].id = i+1;
+        lm[i].numVotos = 0;
+    }
+}
 
+void imprimeMusicas (ListaMusicas *lm){
+    for(int i = 0; i < MAX; i++){
+        printf ("ID: %d\tNumero de votos: %d\n", lm[i].id, lm[i].numVotos);
+    }
+}
 
 void main(){
     int opcao, opcao2, w = 0;
-    Lista *novo, *menorFem = criarLista(), *menorMas = criarLista(), *maiorFem = criarLista(), *maiorMas = criarLista();
     
-    FILE *arq;
-    arq = fopen("save.txt", "r");
+    ListaMusicas musicasMenorFem[MAX], 
+    musicasMaiorFem[MAX], 
+    musicasMenorMas[MAX], 
+    musicasMaiorMas[MAX];
+
+    iniciaListaMusicas(musicasMenorFem);
+    iniciaListaMusicas(musicasMaiorFem);
+    iniciaListaMusicas(musicasMenorMas);
+    iniciaListaMusicas(musicasMaiorMas);
+
+    Lista *novo, 
+    *menorFem = criarLista(), 
+    *menorMas = criarLista(), 
+    *maiorFem = criarLista(), 
+    *maiorMas = criarLista();
 
     while(w == 0){
+
         linha();
         printf("Escolha uma opcao:\n");
         printf("1 - Pesquisa\n");
-        printf("2 - Imprimir\n");
+        printf("2 - Imprimir Pesquisa\n");
+        printf("3 - Imprimir votos das musicas\n");
         linha();
 
         digite();
         scanf("%d", &opcao);
+        limpa();
 
         switch (opcao){
         case 1:
-            novo = CriarEntrada();
+            novo = criarEntrada();
 
             if (novo->info.idade <= 20 && novo->info.sexo == 'F'){
-                AdicionaLista(&menorFem, novo);
+                adicionaLista(&menorFem, novo);
+                for(int i = 0; i < 5; i++){
+                    for(int j = 0; j < MAX; j++){
+                        if(musicasMenorFem[j].id == novo->info.musicas[i]){
+                            musicasMenorFem[j].numVotos++;
+                        }
+                    }
+                }
             }
             else if (novo->info.idade <= 20 && novo->info.sexo == 'M'){
-                AdicionaLista(&menorMas, novo);
+                adicionaLista(&menorMas, novo);
+                for(int i = 0; i < 5; i++){
+                    for(int j = 0; j < MAX; j++){
+                        if(musicasMenorMas[j].id == novo->info.musicas[i]){
+                            musicasMenorMas[j].numVotos++;
+                        }
+                    }
+                }
             }
             else if (novo->info.idade > 20 && novo->info.sexo == 'F'){
-                AdicionaLista(&maiorFem, novo);
+                adicionaLista(&maiorFem, novo);
+                for(int i = 0; i < 5; i++){
+                    for(int j = 0; j < MAX; j++){
+                        if(musicasMaiorFem[j].id == novo->info.musicas[i]){
+                            musicasMaiorFem[j].numVotos++;
+                        }
+                    }
+                }
             }
             else{
-                AdicionaLista(&maiorMas, novo);
+                adicionaLista(&maiorMas, novo);
+                for(int i = 0; i < 5; i++){
+                    for(int j = 0; j < MAX; j++){
+                        if(musicasMaiorMas[j].id == novo->info.musicas[i]){
+                            musicasMaiorMas[j].numVotos++;
+                        }
+                    }
+                }
             }
+            limpa();
             break;
         
         case 2:
@@ -128,6 +219,7 @@ void main(){
             
             digite();
             scanf("%d", &opcao2);
+            limpa();
 
             switch (opcao2){
             case 1:
@@ -150,6 +242,42 @@ void main(){
                 printf("Entrada invalida!!!!");
                 break;
             }
+            break;
+
+        case 3:
+            linha();
+            printf("Digite qual lista imprimir:\n");
+            printf("1 - Feminino/<=20\n");
+            printf("2 - Masculino/<=20\n");
+            printf("3 - Feminino/>20\n");            
+            printf("4 - Masculino/>20\n");
+            
+            digite();
+            scanf("%d", &opcao2);
+            limpa();
+
+            switch (opcao2){
+                case 1:
+                    imprimeMusicas(musicasMenorFem);
+                    break;
+
+                case 2:
+                    imprimeMusicas(musicasMenorMas);
+                    break;
+
+                case 3:
+                    imprimeMusicas(musicasMaiorFem);
+                    break;
+
+                case 4:
+                    imprimeMusicas(musicasMaiorMas);
+                    break;
+                
+                default:
+                    printf("Entrada invalida!!!!");
+                    break;
+                }
+                break;
             break;
        
         default:
